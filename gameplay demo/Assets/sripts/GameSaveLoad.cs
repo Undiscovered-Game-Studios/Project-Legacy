8fe906bf6b1846d27b7e1bcd7f694f508e725b66
DDeathlonger <dantedummer@gmx.com>
2014-07-20 16:03:48 -0700

    Added the menu scripts to demo area player.
    Not finished yet.

---
+++ gameplay demo/Assets/sripts/GameSaveLoad.cs
@@ -0,0 +1,202 @@
+﻿using UnityEngine; 
+using System.Collections; 
+using System.Xml; 
+using System.Xml.Serialization; 
+using System.IO; 
+using System.Text; 
+
+public class GameSaveLoad : MonoBehaviour
+{	
+	// This is our local private members 
+	private float screenHeight;
+	private float screenWidth;
+	private float buttonHeight;
+	private float buttonWidth;
+	bool _ShouldSave, _ShouldLoad,_SwitchSave,_SwitchLoad; 
+	string _FileLocation,_FileName; 
+	public GameObject _Player; 
+	UserData myData; 
+	string _PlayerName; 
+	string _data;
+	int savenumber;
+	int saveSlot;
+
+
+
+/**************************************************************************************************/
+
+
+
+	// When the EGO is instantiated the Start will trigger 
+	// so we set-up our initial values for our local members 
+	public void Start () {
+		screenHeight = Screen.height;
+		screenWidth = Screen.width;
+	
+		buttonHeight = screenHeight * 0.08f;
+		buttonWidth = screenWidth * 0.3f;
+		// Determines what save slot and file number to use.
+		FileInfo t = new FileInfo(_FileLocation+"\\"+ _FileName); 
+		if(!t.Exists)
+		{
+			savenumber =  1;
+		}
+		else if(t.Exists == true && savenumber == 1)
+		{
+			savenumber = 2;
+		}
+		else if(t.Exists == true && savenumber == 2)
+		{
+			savenumber = 3;
+		}
+		else if(t.Exists == true && savenumber == 3)
+		{
+			//functions to handle file overwrite
+		}
+		
+		// Where we want to save and load to and from 
+		_FileLocation=Application.dataPath; 
+		_FileName="SaveData_"; 
+		
+		// we need something to store the information into 
+		myData=new UserData(); 
+	}
+
+
+
+/**************************************************************************************************/
+
+
+
+	//*************************************************** 
+	// Loading The Player... 
+	// **************************************************       
+	public void load() {
+		if(_data.ToString() != "") 
+		{ 
+			// notice how I use a reference to type (UserData) here, you need this 
+			// so that the returned object is converted into the correct type 
+			myData = (UserData)DeserializeObject(_data); 
+			// set the players position to the data we loaded			
+			_Player.transform.position=myData._iUser.v; 
+			_Player.transform.rotation=myData._iUser.q;
+		} 
+	} 
+
+	//*************************************************** 
+	// Saving The Player... 
+	// **************************************************    
+	public void save() {
+		Debug.Log(saveSlot);
+		Debug.Log(saveSlot);
+		Debug.Log(saveSlot);
+		Debug.Log(saveSlot);
+		Debug.Log(saveSlot);
+		myData._iUser.v=_Player.transform.position;
+		myData._iUser.q=_Player.transform.rotation;  
+
+		// Creates XML 
+		_data = SerializeObject(myData); 
+		// This is the final resulting XML from the serialization process 
+		CreateXML();
+	} 
+
+
+
+/**************************************************************************************************/
+
+
+
+	string UTF8ByteArrayToString(byte[] characters) 
+	{      
+		UTF8Encoding encoding = new UTF8Encoding(); 
+		string constructedString = encoding.GetString(characters); 
+		return (constructedString); 
+	} 
+	
+	byte[] StringToUTF8ByteArray(string pXmlString) 
+	{ 
+		UTF8Encoding encoding = new UTF8Encoding(); 
+		byte[] byteArray = encoding.GetBytes(pXmlString); 
+		return byteArray; 
+	} 
+	
+	// Here we serialize our UserData object of myData 
+	string SerializeObject(object pObject) 
+	{ 
+		string XmlizedString = null; 
+		MemoryStream memoryStream = new MemoryStream(); 
+		XmlSerializer xs = new XmlSerializer(typeof(UserData)); 
+		XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8); 
+		xs.Serialize(xmlTextWriter, pObject); 
+		memoryStream = (MemoryStream)xmlTextWriter.BaseStream; 
+		XmlizedString = UTF8ByteArrayToString(memoryStream.ToArray()); 
+		return XmlizedString; 
+	} 
+	
+	// Here we de-serialize it back into its original form 
+	object DeserializeObject(string pXmlizedString) 
+	{ 
+		XmlSerializer xs = new XmlSerializer(typeof(UserData)); 
+		MemoryStream memoryStream = new MemoryStream(StringToUTF8ByteArray(pXmlizedString)); 
+		XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8); 
+		return xs.Deserialize(memoryStream); 
+	} 
+
+
+
+/**************************************************************************************************/
+
+
+
+	// Finally our save and load methods for the file itself 
+	void CreateXML() 
+	{ 
+		StreamWriter writer;
+		FileInfo t = new FileInfo(_FileLocation+"\\"+ _FileName);
+		if(!t.Exists)
+		{
+			writer = t.CreateText();
+		}
+		else if(t.Exists == true && savenumber == 1)
+		{
+			writer = t.CreateText();
+		}
+		else if(t.Exists == true && savenumber == 2)
+		{
+			writer = t.CreateText();
+		}
+		else if(t.Exists == true && savenumber == 3)
+		{
+			if(GUI.Button(new Rect((screenWidth - buttonWidth) * 0.5f, screenHeight * 0.35f, buttonWidth, buttonHeight), "Overwrite ---0"+savenumber+"---?"))
+			{
+				t.Delete();
+				writer = t.CreateText();
+			}
+		}
+	} 
+	
+	public void LoadXML() 
+	{ 
+		StreamReader reader = File.OpenText(_FileLocation+"\\"+ _FileName); 
+		string _info = reader.ReadToEnd(); 
+		reader.Close(); 
+		_data=_info;
+	} 
+} 
+
+// UserData is our custom class that holds our defined objects we want to store in XML format 
+public class UserData 
+{ 
+	// We have to define a default instance of the structure 
+	public DemoData _iUser; 
+	// Default constructor doesn't really do anything at the moment 
+	public UserData() { } 
+	
+	// Anything we want to store in the XML file, we define it here 
+	public struct DemoData 
+	{ 
+		public Vector3 v;
+		public Quaternion q;
+	} 
+}
---
+++ gameplay demo/Assets/sripts/SaveMenu.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveMenu : MonoBehaviour
+{
+    private delegate void MenuDelegate();
+	private MenuDelegate menuFunction;
+	
+	private float screenHeight;
+	private float screenWidth;
+	private float buttonHeight;
+	private float buttonWidth;
+	private float tempVolume;
+	
+	public float volume;
+	
+	public int saveSlot;
+
+	public bool inMenu;
+	
+	public GameSaveLoad saveload;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject player = GameObject.Find("player");
+		//GameSaveLoad saveloadvar = player.GetComponent<GameSaveLoad>();
+		
+		screenHeight = Screen.height;
+		screenWidth = Screen.width;
+	
+		buttonHeight = screenHeight * 0.08f;
+		buttonWidth = screenWidth * 0.3f;
+		
+	//	Screen.lockCursor = true;
+		
+		saveload = (GameSaveLoad) gameObject.GetComponent ("GameSaveLoad");
+	}
+	
+	// Update void used to determine when to open the menu and what to do when the menu is open.
+	
+	void Update()
+	{
+	
+		//Opens menu on input.
+		if (Input.GetKeyDown(KeyCode.Backspace))
+		{
+			menuFunction = mainMenu;
+		}
+		
+		//If Menu is open.
+		if(inMenu)
+		{
+			Time.timeScale = 0f;
+		//	Screen.lockCursor = false;
+			tempVolume = volume;
+			AudioListener.volume = 0f;
+		}
+		
+		//If menu is not open.
+		if(!inMenu)
+		{
+			Time.timeScale = 1f;
+		//	Screen.lockCursor = true;
+			//Determines if volume is pre-set.
+			if(tempVolume > 0)
+			{
+				volume = tempVolume;
+			}
+			AudioListener.volume = volume;
+		}
+	}
+	
+	void OnGUI()
+	{
+		menuFunction();
+	}
+	
+	void mainMenu()
+	{
+		inMenu = true;
+		
+		if(GUI.Button(new Rect((screenWidth - buttonWidth) * 0.5f, screenHeight * 0.5f, buttonWidth, buttonHeight), "Save Game"))
+		{
+			menuFunction = saveGame;
+		}
+		
+		if(GUI.Button(new Rect((screenWidth - buttonWidth) * 0.5f, screenHeight * 0.8f, buttonWidth, buttonHeight), "Resume Game"))
+		{
+			menuFunction = null;
+			inMenu = false;
+		}
+	}
+
+	void saveGame()
+	{
+		GUI.Label(new Rect((screenWidth - buttonWidth) * 0.625f, screenHeight * 0.3f, buttonWidth, buttonHeight), "Save Game");
+		
+		if(GUI.Button(new Rect((screenWidth - buttonWidth) * 0.5f, screenHeight * 0.35f, buttonWidth, buttonHeight), "--- 01 ---"))
+		{
+		//	saveloadvar.saveSlot = 1;
+			saveload.Start();
+			saveload.save();
+		}
+
+		if(GUI.Button(new Rect((screenWidth - buttonWidth) * 0.5f, screenHeight * 0.45f, buttonWidth, buttonHeight), "--- 02 ---"))
+		{
+		//	saveloadvar.saveSlot = 2;
+			saveload.Start();
+			saveload.save();
+		}	
+	
+		if(GUI.Button(new Rect((screenWidth - buttonWidth) * 0.5f, screenHeight * 0.55f, buttonWidth, buttonHeight), "--- 03 ---"))
+		{
+		//	saveloadvar.saveSlot = 3;
+			saveload.Start();
+			saveload.save();
+		}
+		
+		if(GUI.Button(new Rect((screenWidth - buttonWidth) * 0.5f, screenHeight * 0.75f, buttonWidth, buttonHeight), "Main Menu"))
+		{
+			menuFunction = mainMenu;
+		}
+	}
+}
