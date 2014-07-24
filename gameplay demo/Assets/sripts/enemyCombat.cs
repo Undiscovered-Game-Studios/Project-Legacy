a1014546473c42da4d8c74472c6f327ef3cd1275
Patrick Conner <PConner000@gmail.com>
2014-07-24 15:18:31 -0700

    trash

---
+++ gameplay demo/Assets/sripts/enemyCombat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyCombat : MonoBehaviour {
+
+	public GameObject target;
+	public float attack1Damage, attack1Range, attack1Angle, attack1Delay;
+	private float curAttack1Delay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Attack1(){
+
+		}
+}
