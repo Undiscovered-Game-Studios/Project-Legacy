2911ef33c2f51c8fe4a8597b4665e01fa421b52e
Patrick Conner <PConner000@gmail.com>
2014-07-24 15:27:02 -0700

    Revert "trash"
    This reverts commit a1014546473c42da4d8c74472c6f327ef3cd1275.

---
+++ gameplay demo/Assets/sripts/EmenyTriggers/EnemySightTrigger.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySightTrigger : MonoBehaviour {
+	
+	public Transform myTrans, target;
+	
+	public Ray targetRay;
+	public RaycastHit hit;
+	public Vector3 targetDir;
+	
+	public bool canSee = false;
+	
+	void Awake(){
+		myTrans = transform;
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+		target = go.transform;
+	}
+	
+	public void CanSee(){
+		
+		Enemy_AI ea = (Enemy_AI) GetComponent ("Enemy_AI");
+		
+		targetDir = -myTrans.position + ea.target.transform.position;
+		
+		targetRay = new Ray(myTrans.position, targetDir);
+		if(Physics.Raycast(targetRay, out hit)){
+			if(hit.collider.tag == "Player"){
+				canSee = true;
+			}else{
+				canSee = false;
+			}
+		}
+			
+		Debug.DrawRay(myTrans.position, targetDir, Color.red);
+		
+	}
+	
+	public void Trigger(){
+		
+		CanSee();
+		
+		Enemy_AI ea = (Enemy_AI) GetComponent ("Enemy_AI");
+		
+		Vector3 dir = (ea.target.transform.position - transform.position).normalized;
+		
+		if (Vector3.Distance(ea.target.transform.position, myTrans.position) < ea.maxDistance && Vector3.Dot(dir,transform.forward) > 0)
+			if(myTrans.position.y < target.position.y + 5 && myTrans.position.y > target.position.y - 5 /*&& canSee == true*/){
+				ea.isFollowing = true;
+		}
+		
+		if (Vector3.Distance(ea.target.transform.position, myTrans.position) > ea.maxDistance){
+			ea.isFollowing = false;
+		}
+	}
+	
+	void Update(){
+		Trigger();	
+	}
+}
