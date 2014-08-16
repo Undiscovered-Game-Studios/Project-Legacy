623855501091cbd446459b5ed03ca035dae19e7f
Patrick Conner <PConner000@gmail.com>
2014-08-16 12:34:45 -0700

    makes the camera (and whatever else may need to) follow the active player via an empty. all relative positions are dealt through the parenting system. complete

---
+++ gameplay demo/Assets/cameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour {
+
+	public GameObject followedPlayer;
+	public bool copyRotation;
+	
+	// Update is called once per frame
+	void Update () {
+		if (followedPlayer != null) {
+			if (copyRotation == true) {
+				transform.rotation = followedPlayer.transform.rotation;
+			}
+
+			transform.position = followedPlayer.transform.position;
+		}
+	}
+}
