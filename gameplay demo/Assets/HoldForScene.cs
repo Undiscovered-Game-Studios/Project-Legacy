ca8fa475c04dbbea48f0cefa6e2ca496b07e49d9
Patrick Conner <PConner000@gmail.com>
2014-08-16 12:28:34 -0700

    failed attempt at connecting the player characters, please ignore (it does nothing anyway, only didn't delete because doing so caused a compiler error for some reason...)

---
+++ gameplay demo/Assets/HoldForScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HoldForScene : MonoBehaviour {
+	
+	public List<GameObject> heldPlayers;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
