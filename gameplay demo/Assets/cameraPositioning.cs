ca4eb264281bed8438fdb82458b5936e07a6172e
Patrick Conner <PConner000@gmail.com>
2014-08-16 12:30:13 -0700

    failed attempt at setting the camera to follow the active player at a set distance, please ignore (it's not applied anywhere anyway, only didn't delete because doing so caused a compiler error for some reason...)

---
+++ gameplay demo/Assets/cameraPositioning.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraPositioning : MonoBehaviour {
+
+	public float distance, height;
+
+	// Use this for initialization
+	void Start () {
+		distance = Vector3.Distance (transform.position, transform.parent.position);
+		height = transform.position.y - transform.parent.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.parent.position;
+		transform.position += height * transform.up;
+		transform.position -= distance * transform.forward;
+	}
+}
