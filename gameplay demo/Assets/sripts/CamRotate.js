e1a6dbacf4cbb45b61f973be6861a21621dbe29d
Patrick Conner <PConner000@gmail.com>
2014-08-21 14:36:32 -0700

    34 Apparent files that needed committed.
    Don't ask because I only saw these when I opened Git.

---
+++ gameplay demo/Assets/sripts/CamRotate.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+var target : Transform;
+var distance = 5.0;
+var xSpeed = 125.0;
+var rightclicked : boolean = false;
+ 
+private var x = 0.0;
+ 
+@script AddComponentMenu("Camera-Control/Mouse Orbit")
+
+function Start () {
+    var angles = transform.eulerAngles;
+    x = angles.y;
+    target = transform.parent.gameObject.GetComponent(Transform);
+}
+ 
+function Update (){
+    if (Input.GetMouseButtonDown(1)){
+        rightclicked = true;
+        }
+    else{
+        rightclicked = false;
+    }
+}
+ 
+function LateUpdate () {
+    if (target && rightclicked == true) {
+        x += Input.GetAxis("Mouse X") * xSpeed * distance* 0.02;
+           var rotation = Quaternion.Euler(0, x, 0);
+           var position = rotation * Vector3(0.0, 0.0, -distance) + target.position;
+           transform.rotation = rotation;
+           transform.position = position;
+    }   
+}
