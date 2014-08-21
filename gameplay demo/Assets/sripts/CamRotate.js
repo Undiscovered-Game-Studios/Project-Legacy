5c921c53bd209c4cb9b28e3ff1d76cd4b9b5d9b1
Patrick Conner <PConner000@gmail.com>
2014-08-21 16:35:33 -0700

    A JS and CS Cam Rotation script beginning.
    Not done, just a foundation for the script.. need to see player to
    finish.

--- gameplay demo/Assets/sripts/CamRotate.js
+++ gameplay demo/Assets/sripts/CamRotate.js
@@ -1,34 +1,30 @@
 ﻿#pragma strict
-var target : Transform;
-var distance = 5.0;
-var xSpeed = 125.0;
-var rightclicked : boolean = false;
- 
-private var x = 0.0;
- 
-@script AddComponentMenu("Camera-Control/Mouse Orbit")
 
-function Start () {
-    var angles = transform.eulerAngles;
-    x = angles.y;
-    target = transform.parent.gameObject.GetComponent(Transform);
+var rmbDownPoint;
+var mouseRightDrag : boolean = false;
+private var targetPosition : Vector3;
+ 
+// Right Mouse Hold
+if (Input.GetMouseButton(1)) {
+    Screen.showCursor = false;
+    } else {
+    Screen.showCursor = true;
+}
+    //Move mouse left or right with RMB held
+if (Input.GetMouseButtonDown(1))
+{
+    rmbDownPoint = Input.GetAxis;
+    mouseRightDrag = true;
 }
  
-function Update (){
-    if (Input.GetMouseButtonDown(1)){
-        rightclicked = true;
-        }
-    else{
-        rightclicked = false;
-    }
+if (mouseRightDrag)
+{
+    var dragDifference : Vector2 = rmbDownPoint;
+    targetPosition += transform.forward * 27; 
+    transform.RotateAround(targetPosition,Vector3.up+dragDifference ,50*Time.deltaTime);
 }
  
-function LateUpdate () {
-    if (target && rightclicked == true) {
-        x += Input.GetAxis("Mouse X") * xSpeed * distance* 0.02;
-           var rotation = Quaternion.Euler(0, x, 0);
-           var position = rotation * Vector3(0.0, 0.0, -distance) + target.position;
-           transform.rotation = rotation;
-           transform.position = position;
-    }   
+if(Input.GetMouseButtonUp(1))
+{
+    mouseRightDrag = false;
 }
